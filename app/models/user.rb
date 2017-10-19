07e33c1dae9de7486aedab94b24c4796a9aaa694
Olivia Mo <oliviatrademark@gmail.com>
2017-10-20 10:56:30 +1100

    added validation check to sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
 
   has_many :props
   has_one :profile
+
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255},
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
