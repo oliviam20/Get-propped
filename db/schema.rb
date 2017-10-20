241a2525baa42bb3cf10a5d98b581e0233f92d40
Olivia Mo <oliviatrademark@gmail.com>
2017-10-20 14:55:27 +1100

    added description column to props

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019060204) do
+ActiveRecord::Schema.define(version: 20171020035406) do
 
   create_table "profiles", force: :cascade do |t|
     t.string "username"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20171019060204) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
